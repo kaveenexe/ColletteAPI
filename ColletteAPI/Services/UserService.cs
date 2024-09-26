4635a8e2f4403f619e00878510117270879d95a1
Kaveen Uththara <it21108990@my.sliit.lk>
2024-09-26 20:59:48 +0530

    Update Cusomer Logic.
    By default `IsActive: false` for cusomers

--- ColletteAPI/Services/UserService.cs
+++ ColletteAPI/Services/UserService.cs
@@ -32,6 +32,12 @@ namespace ColletteAPI.Services
                 throw new Exception("UserType is not set for this user.");  // Ensures UserType is valid
             }
 
+            // Prevent login if the account is not active
+            if (!user.IsActive)
+            {
+                throw new Exception("Your account is not active. Please contact CSR or Administrator.");
+            }
+
             var token = _jwtService.GenerateToken(user.Id, user.UserType);
 
             return new AuthResponse
@@ -57,7 +63,7 @@ namespace ColletteAPI.Services
                 PasswordHash = _authService.HashPassword(registerDto.Password),
                 UserType = registerDto.UserType,
                 Address = registerDto.Address,
-                IsActive = true
+                IsActive = registerDto.UserType == UserRoles.Customer ? false : true // // Set IsActive to false if the user is a Customer
             };
 
             await _userRepository.AddUser(user);
